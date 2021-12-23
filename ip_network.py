22e59fbd0912c7b8ff8c5ebd93b042e9d856dc0c
elialter <63195898+elialter@users.noreply.github.com>
2021-12-23 23:41:58 +0200

    part 1 without add_noise

--- ip_network.py
+++ ip_network.py
@@ -1,4 +1,8 @@
 from network import *
+import multiprocessing
+import my_queue
+import preprocessor
+import os
 
 class IPNeuralNetwork(NeuralNetwork):
     
@@ -8,15 +12,29 @@ class IPNeuralNetwork(NeuralNetwork):
         '''
         # 1. Create Workers
 		# (Call Worker() with self.mini_batch_size as the batch_size)
-        
-		# 2. Set jobs
-		
+        num_workers = (int)(os.environ['SLURM_CPUS_PER_TASK'])
+        jobs = multiprocessing.JoinableQueue()
+        self.results = my_queue.MyQueue()
+        workers = []
+        for i in range(num_workers):
+            worker = preprocessor.Worker(jobs=jobs, result=self.results,
+                                         training_data=training_data, batch_size=self.mini_batch_size)
+            workers.append(worker)
+            worker.start()
+
+        # 2. Set jobs
+        for i in range(self.number_of_batches * self.epochs):
+            jobs.put('work')
+        for j in workers:
+            jobs.put('stop')
+
         # Call the parent's fit. Notice how create_batches is called inside super.fit().
         super().fit(training_data, validation_data)
-        
+
         # 3. Stop Workers
-        
-        raise NotImplementedError("To be implemented")
+        jobs.join()
+
+#        raise NotImplementedError("To be implemented")
         
         
     
@@ -25,6 +43,13 @@ class IPNeuralNetwork(NeuralNetwork):
         Override this function to return self.number_of_batches batches created by workers
 		Hint: you can either generate (i.e sample randomly from the training data) the image batches here OR in Worker.run()
         '''
-        raise NotImplementedError("To be implemented")
+        batches = []
+        for i in range(self.number_of_batches):
+            temp = self.results.get()
+            batches.append(temp)
+        return batches
+#        raise NotImplementedError("To be implemented")
 
     
+
+
--- preprocessor.py
+++ preprocessor.py
@@ -1,11 +1,19 @@
 import multiprocessing
+import numpy as np
+import random
+from scipy.ndimage import rotate
 
+max_noise = 0.2
 
 class Worker(multiprocessing.Process):
     
     def __init__(self, jobs, result, training_data, batch_size):
         super().__init__()
 
+        self.jobs = jobs
+        self.result = result
+        self.training_data = training_data
+        self.batch_size = batch_size
         ''' Initialize Worker and it's members.
 
         Parameters
@@ -21,7 +29,7 @@ class Worker(multiprocessing.Process):
         
         You should add parameters if you think you need to.
         '''
-        raise NotImplementedError("To be implemented")
+#        raise NotImplementedError("To be implemented")
 
     @staticmethod
     def rotate(image, angle):
@@ -38,7 +46,10 @@ class Worker(multiprocessing.Process):
         ------
         An numpy array of same shape
         '''
-        raise NotImplementedError("To be implemented")
+        processed_image = np.reshape(image, (28, 28))
+        rotate(processed_image, angle, (1, 0), False)
+        return np.reshape(processed_image, 784)
+#        raise NotImplementedError("To be implemented")
 
     @staticmethod
     def shift(image, dx, dy):
@@ -57,7 +68,24 @@ class Worker(multiprocessing.Process):
         ------
         An numpy array of same shape
         '''
-        raise NotImplementedError("To be implemented")
+        shifted_image = np.reshape(image, (28, 28))
+
+        dy *= -1
+        dx *= -1
+        # shifted.show()
+        shifted_image = np.roll(shifted_image, dy, axis=0)
+        shifted_image = np.roll(shifted_image, dx, axis=1)
+        if dx > 0:
+            shifted_image[:, :dx] = 0
+        elif dx < 0:
+            shifted_image[:, dx:] = 0
+        if dy > 0:
+            shifted_image[:dy, :] = 0
+        elif dy < 0:
+            shifted_image[dy:, :] = 0
+
+        return np.reshape(shifted_image, 784)
+#        raise NotImplementedError("To be implemented")
     
     @staticmethod
     def add_noise(image, noise):
@@ -93,7 +121,17 @@ class Worker(multiprocessing.Process):
         ------
         An numpy array of same shape
         '''
-        raise NotImplementedError("To be implemented")
+        n_image = np.reshape(image,(28,28))
+        n_pixels = np.empty_like(n_image)
+        for i in range(n_image.shape[0]):
+            for j in range(n_image.shape[1]):
+                if np.floor(j + i * tilt) < 0 or np.floor(j + i * tilt) > n_image.shape[1] - 1:
+                    n_pixels[i, j] = 0
+                else:
+                    temp = (int)(np.floor(j + i * tilt))
+                    n_pixels[i, j] = np.copy(n_image[i, temp])
+        return np.reshape(n_pixels, 784)
+#        raise NotImplementedError("To be implemented")
 
     def process_image(self, image):
         '''Apply the image process functions
@@ -108,11 +146,38 @@ class Worker(multiprocessing.Process):
         ------
         An numpy array of same shape
         '''
-        raise NotImplementedError("To be implemented")
+        processed_image = np.copy(image)
+
+        for i in range(0,4):
+            augement_function = random.randint(0, 3)
+            if augement_function == 0:
+                processed_image = Worker.add_noise(processed_image,
+                                                   random.uniform(-max_noise, max_noise))
+            if augement_function == 1:
+                processed_image = Worker.shift(processed_image,
+                                               random.randint(-2, 2), random.randint(-2, 2))
+            if augement_function == 2:
+                processed_image = Worker.skew(processed_image, random.uniform(-0.1, 0.1))
+            if augement_function == 3:
+                processed_image = Worker.rotate(processed_image, random.randint(-2, 2))
+
+        return processed_image
+
+#        raise NotImplementedError("To be implemented")
 
     def run(self):
         '''Process images from the jobs queue and add the result to the result queue.
 		Hint: you can either generate (i.e sample randomly from the training data)
 		the image batches here OR in ip_network.create_batches
         '''
-        raise NotImplementedError("To be implemented")
+        while True:
+            job = self.jobs.get()
+            if job == 'stop':
+                self.jobs.task_done()
+                break
+            indexes = random.sample(range(0, len(self.training_data[0])), self.batch_size)
+            images=[self.process_image(self.training_data[0][i]) for i in indexes]
+            t_t = np.array(images), self.training_data[1][indexes]
+            self.result.put(t_t)
+            self.jobs.task_done()
+#        raise NotImplementedError("To be implemented")
